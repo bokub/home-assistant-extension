7c05fe363bfe9d8ace07bc2266ac4deeb2a62485
Boris Kubiak <kubiakboris@gmail.com>
2021-05-05 16:18:19 +0200

    Remove useless plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ReplacePlugin = require('webpack-plugin-replace');
 
 const version = require('./package.json').version;
 
